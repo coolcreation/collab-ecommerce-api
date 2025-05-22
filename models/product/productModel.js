15eef785e321a7c8327d594fe4a88cfb60006392
Jeff <jeffbohn@my.cwi.edu>
2025-05-22 14:33:54 -0600

    alter productRoute attributes slightly, and change API_Specification.md

--- models/product/productModel.js
+++ models/product/productModel.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: false },
-    imageURL: [{ type: String, required: true }],
+    imageURL: [{ type: String, required: false }],  // changing this to false as some items could potential not have an image (digital items/services/warranties?)
     brand: { type: String, required: true },
     price: { type: Number, required: true },
+    stock: { type: Number, required: true },   // jb - adding this to keep track of inventory
     categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
 });
 
