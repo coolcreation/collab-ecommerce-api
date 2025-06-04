035f630858f57ddbe65efa173e67950254703e58
Jeff <jeffbohn@my.cwi.edu>
2025-06-04 07:12:04 -0600

    temporarily disable stripe as we don't have api key and it's crashing Render

--- routes/payment.js
+++ routes/payment.js
@@ -1,6 +1,6 @@
-import express from "express";
-import Stripe from "stripe";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+// import express from "express";
+// import Stripe from "stripe";
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 
 const router = express.Router();
