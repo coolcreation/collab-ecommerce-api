e3f6a5c6a4ef2b4ec4156707eb036138b3463b91
Jeff <jeffbohn@my.cwi.edu>
2025-06-04 07:16:44 -0600

    temporarily disable stripe as we don't have api key and it's crashing Render

--- routes/payment.js
+++ routes/payment.js
@@ -1,4 +1,4 @@
-// import express from "express";
+import express from "express";
 // import Stripe from "stripe";
 // const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -7,17 +7,19 @@ const router = express.Router();
 
 
 // Create payment intent
-router.post("/create-payment-intent", async (req, res) => {
-  const { amount } = req.body;
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: "usd",
-    });
-    res.send({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
-});
+
+//  jb- 6/4: commenting this out until we get .env info as it's currently crashing our Render hosting
+// router.post("/create-payment-intent", async (req, res) => {
+//   const { amount } = req.body;
+//   try {
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount,
+//       currency: "usd",
+//     });
+//     res.send({ clientSecret: paymentIntent.client_secret });
+//   } catch (error) {
+//     res.status(400).send({ error: error.message });
+//   }
+// });
 
 export default router;
